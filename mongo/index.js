bee9af3925e3d7e4d92e77b885296ecdb8207d76
iwin2471 <iwin12372@gmail.com>
2017-05-11 19:17:22 +0900

    [auth] signup in end

--- mongo/index.js
+++ mongo/index.js
@@ -3,31 +3,31 @@ var db = mongoose.connect('mongodb://localhost/cardline');
 mongoose.Promise = global.Promise;
 
 var commnet = mongoose.Schema({
-	card_token: String,
-  	writer_profile: String,
-  	writer: String,
-  	date: String,
-  	summary: String
-})
+	card_token: {type: String},
+  	writer_profile: {type: String},
+  	writer: {type: String},
+  	date: {type: String},
+  	summary: {type: String},
+});
 
 var CardSchema = mongoose.Schema({
-  card_token: Stringm
-  title: String,
-  writer: String,
-  subtitle: String,
-  token: String,
-  like: Number,
-  date: String,
-  token: String
+  card_token: {type: String},
+  title: {type: String},
+  writer: {type: String},
+  subtitle: {type: String},
+  token: {type: String},
+  like: {type: Number},
+  date: {type: String},
+  token: {type: String},
 });
 
 var sync_CardSchema = mongoose.Schema({
-  title: String,
-  subtitle: String,
-  token: String,
-  summary: String,
-  date: String,
-  token: String,
+  title: {type: String},
+  subtitle: {type: String},
+  token: {type: String},
+  summary: {type: String},
+  date: {type: String},
+  token: {type: String},
 });
 
 
@@ -37,11 +37,11 @@ var UsersSchema = mongoose.Schema({
   name: {type: String},
   token: {type: String},
   profile: {type: String},
-  profile_img: {type: String},
+  profile_img: {type: String, default: "http://iwin247.kr:8899/img/default"},
   facebook_id: {type: String},
   interest: {type: String},
   sync: sync_CardSchema,
-  enable_sync: {type: Boolean}
+  enable_sync: {type: Boolean},
   liked_card: [String],
   liked_editor: [String],
   like: {type: Number},
