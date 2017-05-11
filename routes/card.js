3a2ba6d36c14d9fd0c6ef2655e9615d0d09b968a
iwin2471 <iwin12372@gmail.com>
2017-05-11 20:42:46 +0900

    [card] end

--- routes/card.js
+++ routes/card.js
@@ -37,7 +37,8 @@ router.get('/', function(req, res, next) {
             Cards.update({card_token: req.body.card_token}, {$set: {like: cards.like+1}}, (err, result)=>{
               if(err) return res.status(500).send("DB err");
             });
-            Users.update({token: req.body.token}, {$push: {liked_card: cards.card_token}, (err, result)=>{
+
+            Users.update({token: req.body.token}, {$push: {liked_card: cards.card_token}}, (err, result)=>{
               if(err) return res.status(500).send("DB ERr");
               if(result) return res.status(200).send("success");
             });
@@ -65,13 +66,28 @@ router.get('/', function(req, res, next) {
             Cards.update({card_token: req.body.card_token}, {$set: {like: cards.like-1}}, (err, result)=>{
               if(err) return res.status(500).send("DB err");
             });
-            Users.update({token: req.body.token}, {$pop: {liked_card: cards.card_token}, (err, result)=>{
+            Users.update({token: req.body.token}, {$pop: {liked_card: cards.card_token}}, (err, result)=>{
               if(err) return res.status(500).send("DB ERr");
               if(result) return res.status(200).send("success");
             });
           }else return res.status(404).send("card not found");
         });
       }else return res.status(404).send("user not found");
+    });
+  }else{
+    return res.status(400).send("param missing");
+  }
+})
+
+.get("/cards/search/:search", (req, res)=>{
+  var params = ["search"];
+  if(!check_params(new_user, params)){
+    var search = req.params.search;
+
+    Cards.find({$or: [{title: {$regex: search}}, {subtitle: {$regex: search}}, {summary: {$regex: search}}]}, (err, cards)=>{
+      if(err) return res.status(500).send("DB err");
+      if(cards) return res.status(200).json(cards);
+      else return res.status(404).send("not found");
     });
   }else{
     return res.status(400).send("param missing");
