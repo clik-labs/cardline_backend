71fe370b32c09ec92f9b878c62c95959ff6be767
iwin2471 <iwin12372@gmail.com>
2017-05-11 17:05:14 +0900

    [social] add facebook

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/auth', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
