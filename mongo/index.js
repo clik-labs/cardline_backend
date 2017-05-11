535c44b9e59de8393c6df600d8c0ec27d5d475ad
iwin2471 <iwin12372@gmail.com>
2017-05-11 18:43:33 +0900

    [fix]

--- mongo/index.js
+++ mongo/index.js
@@ -2,58 +2,25 @@ var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/cardline');
 mongoose.Promise = global.Promise;
 
-var commnet = mongoose.Schema({
-	card_token: String,
-  writer_profile: String,
-  writer: String,
-  date: String,
-  summary: String
-});
-
 var CardSchema = mongoose.Schema({
-  card_token: String,
-  title: String,
-  writer: String,
-  subtitle: String,
-  token: String,
-  like: Number,
-  date: String,
-  token: String,
-});
-
-var sync_CardSchema = mongoose.Schema({
   title: String,
   subtitle: String,
-  token: String,
   summary: String,
-  date: String,
+  time: String,
   token: String,
+  sync: Boolean
 });
-
 
 var UsersSchema = mongoose.Schema({
-  email: {type: String},
-  passwd: {type: String},
-  name: {type: String},
-  token: {type: String},
-  profile: {type: String},
-  profile_img: {type: String},
-  facebook_id: {type: String},
-  interest: {type: String},
-  sync: sync_CardSchema,
-  enable_sync: {type: Boolean},
+  id: String,
+  passwd: String,
+  name: String,
+  sync: CardSchema,
   liked_card: [String],
-  liked_editor: [String],
-  like: {type: Number},
-  alert: [{
-  	title: {type: String},
-  	summary: {type: String}
-  }]
 });
 
-
 Users = mongoose.model("users", UsersSchema);
-Cards = mongoose.model("Cards", CardSchema);
+Cards = mongooose.model("cards", CardSchema);
+
 exports.Users = Users;
-exports.Cards = Cards;
 exports.db = db;
